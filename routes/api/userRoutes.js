26acf2f3686fe75681fa31c00f84a686d0d87ac6
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-25 21:13:25 -0700

    testing user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,22 @@
 const router = require('express').Router();
 
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController');
+
+// route = /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// route = /api/users/:userId
+router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+// route = /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
