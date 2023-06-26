45db553f11b102fcba998db50953b6b762e8d2f2
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-25 19:27:55 -0700

    added package.json, starting to create models

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must be a valid email address!'],
+        },
+        
+    }
+)
