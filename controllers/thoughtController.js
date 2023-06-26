64c62df88667317292b3d4baa3109edb637b034d
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-25 20:19:05 -0700

    adding file structure for controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    //get all thoughts
+
+    //get single thought by id
+
+    //create new thought
+
+    //update thought
+
+    //delete thought
+
+    //add reaction to a thought
+
+    //remove reaction to a thought
+}
+
+module.exports = thoughtController;
---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    //get all users
+
+    //get single user by id
+
+    //create new user
+
+    //update user
+
+    //delete user
+
+    //add friend to friend list
+
+    //remove friend from friend list
+}
+
+module.exports = userController;
